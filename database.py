50fb403320860f20f642ec3b8650c83cac25b8f9
RafikHachana <rafikhachana@gmail.com>
2021-05-02 17:33:24 +0300

    Basic db utility functions

--- database.py
+++ database.py
@@ -5,4 +5,28 @@ db = client.video_chat_app
 users = db.users
 
 
+# For now, this file is list of utility functions for the database
 
+# todo make the name stored in a local file, so that we don't add a new user to the db everytime the same user connects
+
+def signup(name):
+    user = {
+        'name': name,
+        'online': False
+    }
+    users.insert_one(user)
+
+
+# todo make sure the user doesn't change the online status of other users (do an ip check before)
+def go_online(name, ip):
+    users.update_one({'name': name}, {'$set': {'online': True,
+                                               'ip': ip}})
+
+
+# todo make this exclude the current user
+def get_online_users():
+    return users.find({'online': True})
+
+
+def go_offline(name):
+    users.update_one({'name': name}, {'$set': {'online': False}})
