0bc6cf57a7ed64f81a94de9a41ccc2856de47ea4
RafikHachana <rafikhachana@gmail.com>
2021-05-07 21:02:16 +0300

    Audio debugging changes

--- receive_audio.py
+++ receive_audio.py
@@ -82,10 +82,10 @@ class PlayAudioThread(threading.Thread):
         print("Audio player : Audio stream started.")
         while True:
             global audio_buffer
-            if len(audio_buffer) >= CHUNK * 10:
+            if len(audio_buffer) >= CHUNK * 15:
                 audio_buffer_lock.acquire()
-                stream.write(audio_buffer[:CHUNK])
-                audio_buffer = audio_buffer[CHUNK:]
+                stream.write(audio_buffer[:5*CHUNK])
+                audio_buffer = audio_buffer[5*CHUNK:]
                 audio_buffer_lock.release()
             # Just for debugging (to see if we are having under runs)
             # else:
