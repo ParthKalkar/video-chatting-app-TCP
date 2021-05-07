caf608c114c0ebe1e1e6431a7fe7e4b0cb33d444
RafikHachana <rafikhachana@gmail.com>
2021-05-07 13:57:20 +0300

    Added parallel TCP for ReceiveFrameThread

--- receive_video.py
+++ receive_video.py
@@ -4,109 +4,139 @@ import time
 import pickle
 from datetime import datetime
 
-video_buffer = b""
-video_buffer_lock = threading.Lock()
-frame_size = -1
-frame_size_lock = threading.Lock()
+video_buffer = []
+video_buffer_lock = []
+frame_size = []
+frame_size_lock = []
 tmp_frame_size = []  # to be used when the frame size is changed in the receiver thread but not in the display thread
-tmp_frame_size_lock = threading.Lock()
+tmp_frame_size_lock = []
+
+
+def init_locks_and_buffers(number_of_connections):
+    global video_buffer, video_buffer_lock, frame_size, frame_size_lock, tmp_frame_size, tmp_frame_size_lock
+
+    video_buffer = []
+    frame_size = []
+    tmp_frame_size = []
+
+    video_buffer_lock = []
+    frame_size_lock = []
+    tmp_frame_size_lock = []
+
+    for i in range(number_of_connections):
+        video_buffer.append(b"")
+        frame_size.append(-1)
+        tmp_frame_size.append([])
+
+        video_buffer_lock.append(threading.Lock())
+        frame_size_lock.append(threading.Lock())
+        tmp_frame_size_lock.append(threading.Lock())
+
+
+def new_connection(ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
+    port = 12345
+    s.connect((ip, port))
+    return s
+
+
+def receive_frames(s, connection_id):
+    size = s.recv(1024)
+
+    global frame_size
+    frame_size[connection_id] = int(size.decode('utf-8'))
+
+    print('Video receiver : Frame size in bytes : ' + str(frame_size[connection_id]))
+
+    total_received_bytes = 0
+
+    while True:
+        global video_buffer
+        packet = s.recv(4096)
+        if not packet:
+            print("Video receiver : No packet received !!!! Exiting the child video receiving thread.")
+            break
+
+        total_received_bytes += len(packet)
+        # This part is synchronized with the video server (every 25 frames)
+        # todo consider that the latency is actually way bigger for a frame because it has many packets
+        # todo fix the problem when the hosts don't have the same timezone
+
+        # todo what if this is exactly the size of 25 frames?
+        if total_received_bytes > 25 * frame_size[connection_id]:
+            packet_end = packet[len(packet) - 53:]
+            # print("Video receiver : Received packet seems to be the server time.")
+            # print("Video receiver : Datetime packet length : " + str(len(packet_end)))
+            sending_time = pickle.loads(packet_end)
+            # print("Video receiver : Received server time.")
+            delta = datetime.now() - sending_time
+            latency = abs(delta.total_seconds())  # todo check that the negative values are not actually a problem
+            frame_latency = latency * (frame_size[connection_id] / 4096)
+            # print("Video receiver : Current packet latency : " + str(latency))
+            # print("Video receiver : Estimated video latency : " + str(frame_latency))
+
+            s.sendall(pickle.dumps(latency))  # todo make sure this is the most efficient way to sync
+
+            new_frame_size = s.recv(4096)  # todo make sure it will only receive the new frame size
+            new_frame_size = int(new_frame_size.decode('utf-8'))
+            # print("Video receiver : Frame size changed by server to " + str(new_frame_size))
+            global tmp_frame_size
+            tmp_frame_size_lock[connection_id].acquire()
+            tmp_frame_size[connection_id].append(new_frame_size)  # tmp_frame_size now has the changes of frame size in order.
+            tmp_frame_size_lock[connection_id].release()
+            buffer_string = "NEW_FRAME_SIZE"
+            packet = packet[:len(packet) - 53]
+            video_buffer_lock[connection_id].acquire()
+            video_buffer += packet
+            video_buffer += bytes(buffer_string, 'utf-8')
+            video_buffer_lock[connection_id].release()
+
+            total_received_bytes = 0
+            s.sendall(b"OK")  # Send ACK
+            time.sleep(0.03)
+            continue
+
+        video_buffer_lock[connection_id].acquire()
+        video_buffer[connection_id] += packet
+        video_buffer_lock[connection_id].release()
+        time.sleep(0.01)
+    print('Video receiver : Exiting child video receiving thread.')
+    s.close()
 
 
 class ReceiveFrameThread(threading.Thread):
-    def __init__(self, threadID, name, counter, correspondent_ip):
+    def __init__(self, thread_id, name, counter, correspondent_ip):
         threading.Thread.__init__(self)
-        self.threadID = threadID
+        self.threadID = thread_id
         self.name = name
         self.counter = counter
         self.correspondent_ip = correspondent_ip
 
     def run(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
-        port = 12345
-        IP = self.correspondent_ip
-        s.connect((IP, port))
+        parallel_connections = 5
 
-        print('Connection established (video receiver).')
+        init_locks_and_buffers(parallel_connections)
 
-        # todo add a latency check here
+        connection_threads = []
+        for i in range(parallel_connections):
+            s = new_connection(self.correspondent_ip)
+            new_thread = threading.Thread(target=receive_frames, args=(s, i,))
+            new_thread.start()
+            print("Video receiver : Child thread started.")
+            connection_threads.append(new_thread)
 
-        size = s.recv(1024)
+        # Join the threads
+        for th in connection_threads:
+            th.join()
 
-        global frame_size
-        frame_size = int(size.decode('utf-8'))
+        print("Exiting the main video receiver thread.")
 
-        print('Video receiver : Frame size in bytes : ' + str(frame_size))
-
-        total_received_bytes = 0
-
-        while True:
-            global video_buffer
-            packet = s.recv(4096)
-            if not packet:
-                print("Video receiver : No packet received !!!! Exiting the video receiving thread.")
-                break
-
-            # print("Video receiver : Packet size " + str(len(packet)))
-
-            total_received_bytes += len(packet)
-            # This part is synchronized with the video server (every 25 frames)
-            # todo consider that the latency is actually way bigger for a frame because it has many packets
-            # todo fix the problem when the hosts don't have the same timezone
-
-            # The size of datetime object is 53 bytes (we are assuming that the packet will be exactly that)
-            # if len(packet_end) == 53 and type(pickle.loads(packet)) == datetime:  # todo check this condition
-            # print(total_received_bytes)
-
-            # todo what if this is exactly the size of 25 frames?
-            if total_received_bytes > 25*frame_size:
-                packet_end = packet[len(packet)-53:]
-                print("Video receiver : Received packet seems to be the server time.")
-                print("Video receiver : Datetime packet length : " + str(len(packet_end)))
-                sending_time = pickle.loads(packet_end)
-                print("Video receiver : Received server time.")
-                delta = datetime.now() - sending_time
-                latency = abs(delta.total_seconds())  # todo check that the negative values are not actually a problem
-                frame_latency = latency * (frame_size / 4096)
-                print("Video receiver : Current packet latency : " + str(latency))
-                print("Video receiver : Estimated video latency : " + str(frame_latency))
-
-                s.sendall(pickle.dumps(latency))  # todo make sure this is the most efficient way to sync
-
-                new_frame_size = s.recv(4096)  # todo make sure it will only receive the new frame size
-                new_frame_size = int(new_frame_size.decode('utf-8'))
-                print("Video receiver : Frame size changed by server to " + str(new_frame_size))
-                global tmp_frame_size
-                tmp_frame_size_lock.acquire()
-                tmp_frame_size.append(new_frame_size)  # tmp_frame_size now has the changes of frame size in order.
-                tmp_frame_size_lock.release()
-                buffer_string = "NEW_FRAME_SIZE"
-                # global video_buffer
-                # video_buffer_lock.acquire()
-                packet = packet[:len(packet)-53]
-                video_buffer_lock.acquire()
-                video_buffer += packet
-                # video_buffer_lock.release()
-                video_buffer += bytes(buffer_string, 'utf-8')
-                video_buffer_lock.release()
-
-                total_received_bytes = 0
-                # continue
-                s.sendall(b"OK")  # Send ACK
-                time.sleep(0.03)
-                continue
-
-            video_buffer_lock.acquire()
-            video_buffer += packet
-            video_buffer_lock.release()
-            time.sleep(0.01)
-        print('Video receiver : Exiting video receiving thread.')
-        s.close()
 
 
 class DisplayFrameThread(threading.Thread):
-    def __init__(self, threadID, name, counter):
+    def __init__(self, thread_id, name, counter):
         threading.Thread.__init__(self)
-        self.threadID = threadID
+        self.threadID = thread_id
         self.name = name
         self.counter = counter
 
