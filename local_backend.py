14a0d9d1a204f824432686447f14050da3fb2427
RafikHachana <rafikhachana@gmail.com>
2021-05-08 22:47:30 +0300

    Added the backbone of a Redis-based server

--- local_backend.py
+++ local_backend.py
@@ -1,4 +1,7 @@
 import socket
+import time
+
+import redis
 
 '''
 The backend_server runs in a separate process, and it communicates with:
@@ -15,7 +18,22 @@ The Python processes are mainly:
 '''
 
 
-def backend_server():
+def backend_server_redis():
+    r = redis.Redis()
+    r.set("python_started", True)
+    while 1:
+
+        # Need to implement a good protocol here, will probably be easier then sockets
+
+        close = r.get("quit")
+        if close:
+            break
+
+        # Sleep between polls
+        time.sleep(0.001)
+
+
+def backend_server_socket():
     # Front end socket creation
     front_end = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
     front_end.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
